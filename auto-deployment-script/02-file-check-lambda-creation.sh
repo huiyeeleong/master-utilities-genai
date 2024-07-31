b35d0ba29e0f238f4500f8987161038f92ac5edc
hui.yee.leong <hui.yee.leong@accenture.com>
2024-07-31 21:50:31 +1000

    Updated Stack

--- auto-deployment-script/02-file-check-lambda-creation.sh
+++ auto-deployment-script/02-file-check-lambda-creation.sh
@@ -7,6 +7,8 @@ AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 REGION="us-east-1"
 ECR_REPOSITORY_NAME="file-pages-validation"
 DOCKER_CONTEXT_PATH="../infra/01-file-check"
+STACK_NAME="file-pages-validation"
+TEMPLATE_FILE="file-pages-validation.yaml"
 
 # Create ECR repository if it doesn't exist
 aws ecr describe-repositories --repository-names $ECR_REPOSITORY_NAME > /dev/null 2>&1
@@ -28,6 +30,11 @@ docker tag $IMAGE_NAME:latest $AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR
 docker push $AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY_NAME:latest
 
 # Update the Lambda function to use the new image
-# aws lambda update-function-code --function-name $LAMBDA_FUNCTION_NAME --image-uri $AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY_NAME:latest
+# Deploy CloudFormation stack
+aws cloudformation deploy \
+    --template-file $TEMPLATE_FILE \
+    --stack-name $STACK_NAME \
+    --parameter-overrides ECRRepositoryUri=$AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY_NAME \
+    --capabilities CAPABILITY_IAM
 
-# echo "Lambda function updated with the new Docker image."
+echo "Lambda function deployed with the new Docker image."
