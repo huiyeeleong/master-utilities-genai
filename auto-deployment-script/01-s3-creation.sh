e166a637c6ad1c4ac2650fa3d67c930340752e03
hui.yee.leong <hui.yee.leong@accenture.com>
2024-07-31 21:30:34 +1000

    Updated Stack

--- auto-deployment-script/01-s3-creation.sh
+++ auto-deployment-script/01-s3-creation.sh
@@ -2,10 +2,14 @@
 
 # Prompt the user for the bucket name and region
 read -p "Enter the bucket name: " BUCKET_NAME
-read -p "Enter the AWS region (e.g., us-east-1): " REGION
+read -p "Enter the AWS region (e.g., us-west-2): " REGION
 
 # Create the S3 bucket
-aws s3api create-bucket --bucket $BUCKET_NAME --region $REGION --create-bucket-configuration LocationConstraint=$REGION
+if [ "$REGION" == "us-east-1" ]; then
+    aws s3api create-bucket --bucket $BUCKET_NAME --region $REGION
+else
+    aws s3api create-bucket --bucket $BUCKET_NAME --region $REGION --create-bucket-configuration LocationConstraint=$REGION
+fi
 
 # Check if the bucket was created successfully
 if [ $? -eq 0 ]; then
